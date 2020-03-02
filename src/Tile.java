c5512df1ff29388ea7766488e8adc503ac2dfbab
Al <defef>
2020-03-02 14:42:34 +0000

    Tile Interface renamed

---
+++ src/Tile.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Tile implements TileInterface {
+    @Override
+    public boolean getBuyable() {
+        return false;
+    }
+
+    @Override
+    public void setBuyable(boolean YesOrNo) {
+
+    }
+
+    @Override
+    public ArrayList<Player> getPlayers() {
+        return null;
+    }
+
+    @Override
+    public void addPlayer(Player player) {
+
+    }
+
+    @Override
+    public void removePlayer(Player player) {
+
+    }
+
+    @Override
+    public void getTileName() {
+
+    }
+
+    @Override
+    public void setTileName(String name) {
+
+    }
+
+    @Override
+    public void getTilePos() {
+
+    }
+
+    @Override
+    public void setTilePos(int position) {
+
+    }
+}
