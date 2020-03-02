ea1e0e7f78e7ba52de772dd238d855c709de75a6
Al <defef>
2020-03-02 14:40:14 +0000

    Initial

---
+++ src/Tile.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+interface Tile {
+    boolean getBuyable(); // returns tileBuyable
+    void setBuyable(boolean YesOrNo); // sets tileBuyable to true or false
+    ArrayList<Player> getPlayers(); // returns list of players currently on tile
+    void addPlayer(Player player); // appends player to tilePlayers
+    void removePlayer(Player player); // removes player from tilePlayers
+    void getTileName(); // returns tileName
+    void setTileName(String name); // sets tileName to name
+    void getTilePos(); // returns tilePosition
+    void setTilePos(int position); // Sets tilePosition variable to position of tile in GameBoard
+}
