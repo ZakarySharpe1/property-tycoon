fe02c474ca9d810e55a5fe95634b43380f0f1e56
Al <defef>
2020-03-02 15:22:13 +0000

    Tile interface implemented and documented.

--- src/Tile.java
+++ src/Tile.java
@@ -1,48 +1,99 @@
 import java.util.ArrayList;
 
 public class Tile implements TileInterface {
+
+    private String tileName;
+    private int tilePosition;
+    private boolean isBuyable;
+    private ArrayList<Player> tilePlayers;
+
+    /**
+     * getBuyable returns true if property is buyable and false if it is not.
+     * @return returns isBuyable , Boolean true or false.
+     */
     @Override
     public boolean getBuyable() {
-        return false;
-    }
+        return isBuyable;
 
+    }
+    /**
+     * setBuyable sets isBuyable to true or false.
+     * @param TrueOrFalse wether the property is buyable or not (true if yes, else false).
+     */
     @Override
-    public void setBuyable(boolean YesOrNo) {
+    public void setBuyable(boolean TrueOrFalse) {
+
+        isBuyable = TrueOrFalse;
 
     }
-
+    /**
+     * Returns the Players on the current tile.
+     * @return List of Players on the tile.
+     */
     @Override
     public ArrayList<Player> getPlayers() {
-        return null;
-    }
 
+        return tilePlayers;
+    }
+    /**
+     * Adds player to list of players positioned on the tile.
+     * @param player The Player to be added to the list of players on the tile.
+     */
     @Override
     public void addPlayer(Player player) {
 
+        tilePlayers.add(player);
+
     }
 
+    /**
+     * Removes player from the list of players positioned on the tile.
+     * @param player The Player to remove.
+     */
     @Override
     public void removePlayer(Player player) {
 
-    }
+        tilePlayers.remove(player);
 
+    }
+    /**
+     * Returns the tile name.
+     * @return Tile name.
+     */
     @Override
-    public void getTileName() {
+    public String getTileName() {
+
+        return tileName;
 
     }
-
+    /**
+     * Sets the tile name.
+     * @param name The desired name of the tile.
+     */
     @Override
     public void setTileName(String name) {
 
+        tileName = name;
+
     }
+    /**
+     * Returns the position of the tile on the game board.
+     * @return int number of position of tile on the board.
+     */
 
     @Override
-    public void getTilePos() {
+    public int getTilePos() {
 
+        return tilePosition;
     }
-
+    /**
+     * Sets the value of the position of the tile on the game board
+     * @param position The desired position.
+     */
     @Override
     public void setTilePos(int position) {
+
+        tilePosition = position;
 
     }
 }
--- src/TileInterface.java
+++ src/TileInterface.java
@@ -6,8 +6,8 @@ interface TileInterface {
     ArrayList<Player> getPlayers(); // returns list of players currently on tile
     void addPlayer(Player player); // appends player to tilePlayers
     void removePlayer(Player player); // removes player from tilePlayers
-    void getTileName(); // returns tileName
+    String getTileName(); // returns tileName
     void setTileName(String name); // sets tileName to name
-    void getTilePos(); // returns tilePosition
+    int getTilePos(); // returns tilePosition
     void setTilePos(int position); // Sets tilePosition variable to position of tile in GameBoard
 }
