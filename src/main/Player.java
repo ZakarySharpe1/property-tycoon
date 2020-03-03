8aa2322765cea83f1e3b835ed28b9a7014ff3f17
kacpe <kacpernowacki999@gmail.com>
2020-03-03 13:16:19 +0000

    Player class basic implementation, comments to be added and changes to be made to a couple of methods

--- src/main/Player.java
+++ src/main/Player.java
@@ -14,24 +14,39 @@ public class Player implements PlayerInterface {
 
     public Player(){
         getOfJailCards = 0;
-
     }
 
+    /**
+     * getProperties returns an array list with all the properties that the player owns
+     * @return returns properties, ArrayList with all the properties player owns
+     */
     @Override
     public ArrayList<Tile> getProperties(){
         return properties;
     }
 
+    /**
+     * getMoney returns an integer that represents the current balance of the player (how much money he has)
+     * @return returns balance as an integer
+     */
     @Override
     public int getMoney(){
         return balance;
     }
 
+    /**
+     * getPlayerTile will return the object tile that the player is currently standing on
+     * @return returns position as an tile object
+     */
     @Override
     public Tile getPlayerTile(){
         return position;
     }
 
+    /**
+     * getOutOfJailFree will return true if player has at least one card and false otherwise
+     * @return returns true or false depending if "get out of jail" card is present or not
+     */
     @Override
     public boolean getOutOfJailFree(){
         if(getOfJailCards > 0){
@@ -56,6 +71,10 @@ public class Player implements PlayerInterface {
         }
     }*/
 
+    /**
+     * getPlayerToken will return the game piece that the player is using in the current game
+     * @return returns gameToken as a string
+     */
     @Override
     public String getPlayerToken(){
         // This needs work depending on how we want to implement game pieces
