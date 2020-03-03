20ae73b6be3d1c53abecd8eb8e5f57235cdb2142
Al <defef>
2020-03-03 15:47:07 +0000

    Code refactor

--- src/Tiles/CommunityChest.java
+++ src/Tiles/CommunityChest.java
@@ -1,4 +1,7 @@
 package Tiles;
 
 public class CommunityChest {
+
+
+
 }
--- src/main/ConfigReader.java
+++ src/main/ConfigReader.java
@@ -1,20 +1,18 @@
 package main;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigReader {
 
-    private ArrayList<Map> tileList ;
+    private ArrayList<Map> tileList;
 
     public ArrayList<Map> ConfigReader() {
 
@@ -22,7 +20,7 @@ public class ConfigReader {
         tileList = new ArrayList<>();
         try {
 
-            File file = new File("resources/config/config.xml");
+            File file = new File("resources/config/property-property-config.xml");
 
             DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
@@ -42,12 +40,50 @@ public class ConfigReader {
 
                     Element eElement = (Element) nNode;
 
+                    ArrayList<String> elements = new ArrayList<>();
 
+                    try {
+                        switch (eElement.getElementsByTagName("type").item(0).getTextContent()) {
 
-                    String[] elements = {"name", "type", "cost", "one-house", "two-house", "three-house", "four-house", "hotel", "colour"};
-                    for(String ele : elements){
-                        dictionary.put(ele,eElement.getElementsByTagName(ele).item(0).getTextContent());
-                    };
+                            case "property":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "one-house", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "go":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "one-house", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "jail":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "tax":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "chest":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "station":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "parking":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "gotojail":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+                            case "random":
+                                elements.addAll(Arrays.asList("name", "type", "cost", "two-house", "three-house", "four-house", "hotel", "colour"));
+                                break;
+
+                        }
+
+                        for (String ele : elements) {
+                            dictionary.put(ele, eElement.getElementsByTagName(ele).item(0).getTextContent());
+                        }
+
+                    } catch (Exception e) {
+
+                        System.out.println("error in config file setup");
+
+                    }
 
 
                     tileList.add(dictionary);
