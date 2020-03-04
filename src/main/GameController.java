44c8726449d3f9dbdc3571876a29c9cb22f17f2c
Al <defef>
2020-03-04 15:41:33 +0000

    config change

--- src/main/GameController.java
+++ src/main/GameController.java
@@ -1,8 +1,10 @@
 package main;
 
-public interface GameController {
-    void endGame(); //ends game if called by setting gameRunning Boolean instance variable to false
-    int rollDice(); //returns a random integer between 1-12
-    int getPotLuck(); //returns an int to use in a switch statement that carries out respective potLuck functionality
-    int getOpportunityKnocks(); //returns an int to use in a switch statement that carries out respective opportunityKnocks functionality
+public class GameController implements GameControllerInterface {
+
+    @Override
+    public void endGame() {
+
+    }
+
 }
---
+++ src/main/GameControllerInterface.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface GameControllerInterface {
+    void endGame(); //ends game if called by setting gameRunning Boolean instance variable to false
+}
