7c0a8fcb16098d1d4b76fcc56f3f9e2bf61a06c5
kacpe <kacpernowacki999@gmail.com>
2020-03-20 16:33:41 +0000

    Dice class implementation along with the comments

--- src/main/Dice.java
+++ src/main/Dice.java
@@ -8,6 +8,9 @@ class Dice implements DiceInterface {
     private int d1;
     private int d2;
 
+    /**
+     * reset sets all three variables to their initial state
+     */
     @Override
     public void reset(){
         counter = 0;
@@ -15,7 +18,11 @@ class Dice implements DiceInterface {
         d2 = 0;
     }
 
-
+    /**
+     * rollDice returns true if its a double (both rolls come out with the same number) and false otherwise.
+     * With each roll counter is incremented by one
+     * @return returns true of false depending if the two rolls come out with the same number
+     */
     @Override
     public boolean rollDice(){
         Random r = new Random();
@@ -32,7 +39,10 @@ class Dice implements DiceInterface {
         }
     }
 
-
+    /**
+     * jailCheck will return true if both rolls are the same number and the counter is equal to three and false otherwise.
+     * @return returns true or false depending if player rolled three doubles or not
+     */
     @Override
     public boolean jailCheck(){
         if((d1 == d2) && (counter == 3)){
@@ -43,7 +53,10 @@ class Dice implements DiceInterface {
         }
     }
 
-
+    /**
+     * getValue will return the summed up value of the two values rolled by the dices
+     * @return returns the total value of the two dices
+     */
     @Override
     public int getValue(){
         return d1+d2;
